bdab20b531e2f06af5ee1e1fd959cb69c2df608e
vannamm1233 <beratbulakpun0suy12@hotmail.com>
2024-05-07 13:30:52 +0700

    First commit

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const dropWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[0])) arr = arr.slice(1);
-  return arr;
-};
+function combinationSum3(k, n) {
+  const result = [];
+  backtrack([], 1, k, n);
+  return result;
+  function backtrack(combination, start, k, n) {
+    if (n === 0 && k === 0) {
+      result.push([...combination]);
+      return;
+    }
+    if (n < 0 || k === 0) return;
+    for (let i = start; i <= 9; i++) {
+      combination.push(i);
+      backtrack(combination, i + 1, k - 1, n - i);
+      combination.pop();
+    }
+  }
+}
