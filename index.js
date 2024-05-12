35813f5c1c7123b376cb06620d43845299ecc4d6
vannamm1233 <beratbulakpun0suy12@hotmail.com>
2024-05-13 06:26:04 +0700

    test

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const aliceBalanceAfter = await aptos.getAccountCoinsData({
-  accountAddress: aliceAddress,
-  minimumLedgerVersion: BigInt(response.version),
-});
-const bobBalanceAfter = await aptos.getAccountCoinsData({
-  accountAddress: bobAddress,
-});
-const sponsorBalanceAfter = await aptos.getAccountCoinsData({
-  accountAddress: sponsorAddress,
-});
+const name = "john";
+const upperName = name.toUpperCase();
+console.log(upperName); // JOHN
